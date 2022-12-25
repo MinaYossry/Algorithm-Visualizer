5a7d51de048397a25b488bf84bbc960350f81549
Mina Yossry <mina.yossry4@gmail.com>
2022-12-25 18:38:00 +0200

    hotfix background-color

--- Sort.js
+++ Sort.js
@@ -3,8 +3,8 @@ function Sort() {
     var calc = new Calcualtion();
 
     this.bubbleSort3 = function (arr) {
-        var i = 0, j = 0;
-        var sorted = true;
+        let i = 0, j = 0;
+        let sorted = true;
         interval = setInterval(function () {
             view.glowView(j, 'green');
             view.glowView(j - 1, 'green');
@@ -19,15 +19,24 @@ function Sort() {
             }
             j++;
             if (j == arr.length - i - 1) {
-                view.glowView(j, 'yellow')
-                view.glowView(j - 1, 'green')
-                i++;
-                j = 0
-                if (i == arr.length - 1 || (sorted)) {
+                if (sorted) {
                     clearInterval(interval);
                     $("#graph div").animate({
-                        'background-color': "green"
+                        'background-color': "blue"
                     }, 100);
+                }
+                else {
+                    view.glowView(j, 'yellow')
+                    view.glowView(j - 1, 'green')
+                    i++;
+                    j = 0;
+                    sorted = true;
+                    if (i == arr.length - 1) {
+                        clearInterval(interval);
+                        $("#graph div").animate({
+                            'background-color': "blue"
+                        }, 100);
+                    }
                 }
             }
         }, 600);
