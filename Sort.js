01daac4078be5fdfbb8cbc4f0d721d3aabbf6eaf
Mina Yossry <mina.yossry4@gmail.com>
2022-12-25 19:14:55 +0200

    working v2.0

--- Sort.js
+++ Sort.js
@@ -6,8 +6,8 @@ function Sort() {
         let i = 0, j = 0;
         let sorted = true;
         interval = setInterval(function () {
-            view.glowView(j, 'green');
-            view.glowView(j - 1, 'green');
+            view.glowView(j, '#678983');
+            view.glowView(j - 1, '#678983');
             view.glowView(j, 'red');
             view.glowView(j + 1, 'red');
 
@@ -22,19 +22,19 @@ function Sort() {
                 if (sorted) {
                     clearInterval(interval);
                     $("#graph div").animate({
-                        'background-color': "blue"
+                        'background-color': "#181D31"
                     }, 100);
                 }
                 else {
-                    view.glowView(j, 'yellow')
-                    view.glowView(j - 1, 'green')
+                    view.glowView(j, 'rgb(200 94 21)')
+                    view.glowView(j - 1, '#678983')
                     i++;
                     j = 0;
                     sorted = true;
                     if (i == arr.length - 1) {
                         clearInterval(interval);
                         $("#graph div").animate({
-                            'background-color': "blue"
+                            'background-color': "#181D31"
                         }, 100);
                     }
                 }
