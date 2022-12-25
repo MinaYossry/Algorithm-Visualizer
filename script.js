57a19a7950048001431c3e5d2a7af7c0405e1ed7
Mina Yossry <mina.yossry4@gmail.com>
2022-12-25 08:07:34 +0200

    working v1

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var swapping = false;
 var FIXED = "n";
 var ELEMENTWIDTH = 50;
+var arr;
 
 $(function () {
+    arr = getRandomArr();
+    generateDivs(arr);
+
     $(".graph div").each(function (idx, elem) {
         elem.style.height = (((parseInt(elem.innerText) / 50) * 150) + 50) + "px";
     });
@@ -67,48 +71,97 @@ function calcHeight(number, maxValue) {
 }
 
 
-var count = 0;
+
 function swapView(inedx1, index2) {
     // Get A strings of id of both elements
-    console.log(++count);
     var elem1 = $("#" + FIXED + inedx1);
     var elem2 = $("#" + FIXED + index2);
-    console.log(elem1, elem2);
-
+    console.log(inedx1, elem1, index2, elem2);
     if (!swapping) {
+
         swapping = true;
 
         var pos1 = parseInt(elem1.css("left"));
         var pos2 = parseInt(elem2.css("left"));
 
+        elem1.attr("id", FIXED + index2)
+        elem2.attr("id", FIXED + inedx1)
+
         // swap left position of both elements
         elem1.animate({
             left: `${pos2}px`
-        }, 0, "linear");
+        }, 500, "linear");
         elem2.animate({
             left: `${[pos1]}px`
-        }, 0, "linear", () => (swapping = false));
+        }, 500, "linear", function () {
+            swapping = false;
+        });
     }
 }
 
 
+
+function test() {
+    var all = $("#graph div");
+    for (let i = 0; i < all.length - 1; i++) {
+        for (let j = 0; j < all.length - i - 1; j++) {
+            if (parseInt(all.get(j).innerText) > parseInt(all.get(j + 1).innerText)) {
+                setTimeout(() => {
+                    console.log(parseInt(all.get(j).innerText), parseInt(all.get(j + 1).innerText));
+                    swapView(j, j + 1);
+                }, 1000 * (j + 1) * (i + 1));
+            }
+        }
+    }
+}
+
+var funArr = [];
 function bubbleSort(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        for (let j = 0; j < arr.length - i; j++) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 setTimeout(() => {
                     swapView(j, j + 1);
-                }, 200 * (j + 1) * (i + 1));
+                }, 500 * (j + 1) * (i + 1));
                 swapNumber(arr, j, j + 1);
             }
-
         }
 
     }
 }
+
+var operation = [];
+var all = $("#graph div");
+
+function bubbleSort2(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                operation.push([j, j + 1])
+                swapNumber(arr, j, j + 1);
+            }
+        }
+
+    }
+    doOP();
+}
+
+var opIndex = 0;
+var interval;
+function doOP() {
+    interval = setInterval(() => {
+        console.log(operation[opIndex]);
+        swapView(operation[opIndex][0], operation[opIndex][1])
+        opIndex++;
+        if (opIndex == operation.length)
+            clearInterval(interval);
+    }, 600);
+}
+
 
 function swapNumber(arr, index1, index2) {
     var temp = arr[index1];
     arr[index1] = arr[index2];
     arr[index2] = temp;
 }
+
