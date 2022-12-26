ce6afe19623b898a19b98997ee84e9a38865e251
Mina Yossry <mina.yossry4@gmail.com>
2022-12-26 16:26:17 +0200

    v2.1: working buttons and refactor

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-
-
-var arr;
-var view = new View();
-var calc = new Calcualtion();
-var sort = new Sort();
 $(function () {
-    arr = calc.geterateRandomArr();
-    view.generateDivs(arr);
-    sort.bubbleSort3(arr);
+    var view = new View();
+    var calc = new Calcualtion();
+    var sort = new Sort();
+
+    calc.generateRandomArr();
+    view.generateDivs(calc.generatedArr, calc.maxValue);
+
+    $("#random").click(function () {
+        calc.generateRandomArr();
+        view.generateDivs(calc.generatedArr, calc.maxValue);
+    });
+
+    $("#startSort").click(function () {
+        sort.bubbleSort(calc.generatedArr, view, calc);
+    });
+
+    $("#enterNumbers").click(function () {
+        var arr = $("#userNumbers").val().split(",");
+        console.log(arr);
+        calc.enterArray(arr);
+        view.generateDivs(calc.generatedArr, calc.maxValue);
+    });
 })
+
 
 
 
